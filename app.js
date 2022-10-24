71cc4c4e0b724dcd4d0d5acaab45775038e2b5fa
obedi bedjo <obedindevu@gmail.com>
2022-10-24 15:42:44 +0200

    feat: creating of index.html

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { request } = require('express');
 const express= require('express');
 const app = express();
+const path = require('path');
+
+let pool = new Pool();
+
 const morgan = require('morgan');
 const {Pool}= require('pg');
 require('dotenv').config();
@@ -13,8 +17,20 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.send('hello world');
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
+app.get('/info/get',(req,res)=>{
+    try {
+        Pool.connect((err, client, release)=>{
+
+        });
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+});
 
 app.listen(port,[
     console.log('le serveur est en ecoute au port 3000')
